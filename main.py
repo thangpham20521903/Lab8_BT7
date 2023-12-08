47deb61ccfe959aa83cbfd59347246f758414136
thangpham20521903 <78943642+thangpham20521903@users.noreply.github.com>
2023-12-08 16:13:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
 
+class_list = {'1': 'Male', '0': 'Female'}
+
+st.title('Name Prediction')
+input = open('ec_vinames.pkl', 'rb')
+encoder = pkl.load(input)
+
+input = open('lrc_vinames.pkl', 'rb')
+model = pkl.load(input)
+
+st.header('Write Name')
+txt = st.text_area("","")
+                         
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.transform([txt])
+    label = str((model.predict(feature_vector))[0])
+    st.header('Result')
+    st.text(class_list[label])
